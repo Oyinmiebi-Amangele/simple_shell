7937c511ed509c61f7623cbade94c269048322d0
Oyinmiebi Amangele <miebi.amangele@gmail.com>
2023-07-26 15:12:36 +0000

    made argv unsused in main.c

--- main.c
+++ main.c
@@ -22,15 +22,12 @@ void sig_handler(int sig)
  *
  * Return: The return value of the last executed command.
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int ret = 0, retn, hist;
+	int ret = 0, retn;
 	int *exe_ret = &retn;
 	char *pmt = "watts $ ", *new_line = "\n";
 
-	char *name = argv[0];
-
-	hist = 1;
 	signal(SIGINT, sig_handler);
 
 	*exe_ret = 0;
